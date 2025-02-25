ffb502db83659b5e47e13f031751c772ca6baaf3
UtnValure <vlaure@alu.frlp.utn.edu.ar>
2025-02-25 00:37:33 -0300

    JWT

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,96 +1,93 @@
-//el contexto de autenticacion se encargue de almacenar y gestionar la información del usuario logueado, 
-//incluyendo su rol.
 import React, { createContext, useState, useEffect } from 'react';
-import { login, logout } from '../helpers/usuarios/loginRegisterLogout';
-
+import { useNavigate } from 'react-router-dom';
+import { isTokenExpired } from '../utils/auth'; // Importa la función isTokenExpired
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null); 
-    const [userId, setUserId] = useState(null); 
-    const [role, setRole] = useState(null); 
-    const [auth, setAuthenticated] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [user, setUser] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [role, setRole] = useState(null);
+  const [auth, setAuthenticated] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
+  // Verifica si el token ha expirado cada vez que el componente se monta o cuando el token cambia
+  useEffect(() => {
+    const storedAuthorization = localStorage.getItem("authorization");
 
-    useEffect(() => {
-        const verificarAutenticacion = async () => {
-            const storedUser = localStorage.getItem("user");
-            const storedRole = localStorage.getItem("userRole");
-            const storedId = localStorage.getItem("userId");
-            const storedAuthorization = localStorage.getItem("authorization");
-
-
-            if (storedUser) {
-                setUser(JSON.parse(storedUser)); 
-            }
-
-            if (storedRole) {
-                setRole(storedRole); 
-            }
-
-            if (storedId) {
-                setUserId(parseInt(storedId));
-            }
-            setLoading(false);
-            if (storedAuthorization){
-                setAuthenticated(storedAuthorization)
-            }
-        };
-
-        verificarAutenticacion();
-    }, []);
-
-    const iniciarSesion = async (userData) => {
-        setLoading(true);
-        setError(null);
-        try {
-            const userInfo = await login(userData);
-            console.log("estoy en inicio de sesion", userInfo);
-
-            localStorage.setItem("user", JSON.stringify(userInfo.userData));
-            localStorage.setItem("userId", userInfo.userData.id);
-            localStorage.setItem("userRole", userInfo.userData.rol);
-            localStorage.setItem("authorization", userInfo.userData.token);
-
-            setUser(userInfo.userData); 
-            setUserId(parseInt(userInfo.userData.id));
-            setRole(userInfo.userData.rol); 
-            setAuthenticated(userInfo.userData.token);
-            return true;
-        } catch (error) {
-            console.error('Error en el login:', error);
-            setError('Usuario o contraseña incorrectos');
-            return false;
-        }finally {
-            setLoading(false);
+    if (storedAuthorization) {
+      const checkToken = async () => {
+        const expired = await isTokenExpired(storedAuthorization);
+        if (expired) {
+          console.log("Token expirado, cerrando sesión...");
+          setAuthenticated(null); // Limpia el estado de autenticación
+          setUser(null);
+          setUserId(null);
+          setRole(null);
+          localStorage.removeItem("authorization"); // Elimina el token del localStorage
+          localStorage.removeItem("user");
+          localStorage.removeItem("userId");
+          localStorage.removeItem("userRole");
+          navigate('/login'); // Redirige al usuario a la pantalla de inicio de sesión
+        } else {
+          setAuthenticated(storedAuthorization); // Actualiza el estado con el token válido
         }
-    };
+      };
 
-    const cerrarSesion = async () => {
-        setLoading(true);
-        try {
-            await logout(); 
-            setUser(null);
-            setUserId(null);
-            setRole(null); 
-            setAuthenticated(null);
-            localStorage.removeItem("userRole");
-            localStorage.removeItem("userId");
-            localStorage.removeItem("user");
-            localStorage.removeItem("authorization");
-        } catch (error) {
-            console.error('Error en el logout:', error);
-        }finally{
-            setLoading(false);
-        }
-    };
+      checkToken();
+    }
+    setLoading(false);
+  }, [navigate]);
 
-    return (
-        <AuthContext.Provider value={{ user, userId, role, auth, loading, iniciarSesion, cerrarSesion, error }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  const iniciarSesion = async (userData) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const userInfo = await login(userData);
+
+      localStorage.setItem("user", JSON.stringify(userInfo.userData));
+      localStorage.setItem("userId", userInfo.userData.id);
+      localStorage.setItem("userRole", userInfo.userData.rol);
+      localStorage.setItem("authorization", userInfo.userData.token);
+
+      setUser(userInfo.userData);
+      setUserId(parseInt(userInfo.userData.id));
+      setRole(userInfo.userData.rol);
+      setAuthenticated(userInfo.userData.token); // Actualiza el estado auth
+      return true;
+    } catch (error) {
+      console.error('Error en el login:', error);
+      setError('Usuario o contraseña incorrectos');
+      return false;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const cerrarSesion = async () => {
+    setLoading(true);
+    try {
+      await logout();
+      setUser(null);
+      setUserId(null);
+      setRole(null);
+      setAuthenticated(null); // Limpia el estado auth
+      localStorage.removeItem("userRole");
+      localStorage.removeItem("userId");
+      localStorage.removeItem("user");
+      localStorage.removeItem("authorization");
+    } catch (error) {
+      console.error('Error en el logout:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, userId, role, auth, loading, iniciarSesion, cerrarSesion, error }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
--- src/helpers/autenticacion/tokenValidation.js
+++ src/helpers/autenticacion/tokenValidation.js
@@ -1,26 +1,26 @@
-export async function isTokenExpired(token) {
-  if (!token) return true;
-
-  try {
+export function isTokenExpired(token) {
+    if (!token) return true;
+  
+    try {
       const parts = token.split('.');
       if (parts.length !== 3) {
-          throw new Error("El token no tiene el formato correcto");
+        throw new Error("El token no tiene el formato correcto");
       }
-
+  
       // Decodifica el payload del token
       const payload = JSON.parse(atob(parts[1].replace(/-/g, '+').replace(/_/g, '/')));
-
+  
       if (!payload.exp) {
-          throw new Error("El token no tiene un campo de expiración (exp)");
+        throw new Error("El token no tiene un campo de expiración (exp)");
       }
-
+  
       // Convierte el tiempo de expiración a milisegundos
       const expirationTime = payload.exp * 1000;
-
+  
       // Verifica si ha expirado
       return Date.now() >= expirationTime;
-  } catch (error) {
+    } catch (error) {
       console.error("Error al decodificar el token:", error);
       return true; // Si hay un error, considera el token como expirado
+    }
   }
-}
